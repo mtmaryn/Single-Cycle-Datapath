9a8ea60f8c51146b67eb8b422644e64ba7bbcf81
i-abdi <i.abdi@hotmail.com>
2016-11-23 13:39:34 -0800

    Update Add2Com.v

--- Add2Com.v
+++ Add2Com.v
@@ -2,18 +2,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 //Subtraction can be used as a comparator
 //////////////////////////////////////////////////////////////////////////////////
-module Add2Com( A, B, Cin, Sum, Cout);
+module Add2Com( A, B, Cin, neg, Sum, Cout);
 	input  [15:0]  A, B;
 	input  Cin;
+	input neg;
 	output [15:0] Sum; 
 	output Cout;	
 	
 	wire [15:0] B2C;
 	wire	temp;
 	
-	
+	xor xor1(B2C,B,neg);
 	two_comp two1( .X(B), .Y(B2C));	
-	Add Add1( .A(A), .B(B2C), .Cin(1'b1), .Sum(Sum), .Cout(temp));
+	Add Add1( .A(A), .B(B2C), .Cin(neg), .Sum(Sum), .Cout(temp));
 	assign Cout = temp;
 
 endmodule
